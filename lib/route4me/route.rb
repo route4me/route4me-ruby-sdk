013ee57a761397de4a76666edb2c38dac0bdf50d
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2014-07-06 23:37:08 +0300

    remove api key

--- lib/route4me/route.rb
+++ lib/route4me/route.rb
@@ -4,7 +4,7 @@ module Route4me
       '/api.v4/route.php'
     end
 
-    def self.get(params={}, api_key=nil)
+    def self.get(params={})
       get = Util.extract(params, %i(
         route_id directions route_path_output
         device_tracking_history limit offset

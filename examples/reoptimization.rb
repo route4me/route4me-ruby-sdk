b897dcfc3b0a2b3332d8b378eb4694c338ecae11
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2014-08-04 15:59:58 +0300

    fix

--- examples/reoptimization.rb
+++ examples/reoptimization.rb
@@ -3,7 +3,7 @@ require './helper'
 
 problem = Route4me::OptimizationProblem.update(
   :optimization_problem_id => 'F2FEA85DA7EFCE180CAD70704816347A',
-  :parameters => { :disable_optimization => true },
+  :parameters => { :reoptimize => true },
   :addresses => addresses,
   :reoptimize => true
 )

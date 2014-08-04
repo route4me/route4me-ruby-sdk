6b0b9a8e7a69b226a2224b76b09bbce3a0d9ac59
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2014-08-04 18:44:05 +0300

    remove require

---
+++ spec/route4me/track_spec.rb
@@ -0,0 +1,33 @@
+describe Route4me::Track do
+  it "Should track device" do
+    status = Route4me::Track.set(
+      'format'      => 'json',
+      'member_id'   => 1,
+      'route_id'    => '0CDA1358186D616AFD39FEB579A64974',
+      'course'      => 1,
+      'speed'       => 120,
+      'lat'         => 41.8927521,
+      'lng'         => -109.0803888,
+      'device_type' => 'iphone',
+      'device_guid' => 'qweqweqwe'
+    )
+
+    status == true
+  end
+
+  it "Should track device with sym" do
+    status = Route4me::Track.set(
+      format: 'json',
+      member_id: 1,
+      route_id: '0CDA1358186D616AFD39FEB579A64974',
+      course: 1,
+      speed: 120,
+      lat: 41.8927521,
+      lng: -109.0803888,
+      device_type: 'iphone',
+      device_guid: 'qweqweqwe'
+    ) 
+
+    status == true
+  end
+end

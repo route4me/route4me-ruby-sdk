dd2c6cb262aded5b8ae39a21bb476b66962b412b
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2014-08-04 19:03:42 +0300

    add symbolize_names method

--- lib/route4me/util.rb
+++ lib/route4me/util.rb
@@ -8,5 +8,21 @@ module Route4me
     def self.url_encode(key)
       URI.escape(key.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
     end
+
+    def self.symbolize_names(object)
+      case object
+      when Hash
+        new = {}
+        object.each do |key, value|
+          key = (key.to_sym rescue key) || key
+          new[key] = symbolize_names(value)
+        end
+        new
+      when Array
+        object.map { |value| symbolize_names(value) }
+      else
+        object
+      end
+    end
   end
 end
